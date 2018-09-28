236a98af3a31fa17e77bfa4412f95fa5d6831232
Jesús Said Llamas Manriquez <saidl14s@users.noreply.github.com>
2018-09-28 18:08:13 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+* Return a new promise whose fulfillment value is revealed only
+* after ms milliseconds
+* @param {number} ms milliseconds
+* @returns {Promise}
+*/
+function test(){
+  alert('Hello!');
+}
