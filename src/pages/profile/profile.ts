5d23afafb4465ea9fd4d0b67571c8308db5b16b9
Jesús Said Llamas Manriquez <saidllamas@MacBook-Air-de-Jesus.local>
2018-09-30 21:03:18 -0500

    Added MD5 and Gravatar for profile images

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import md5 from 'crypto-md5';
 
 /**
  * Generated class for the ProfilePage page.
@@ -14,12 +15,15 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
-
+  email: any;
+  password: any;
+  profilePicture: any = "https://www.gravatar.com/avatar/"
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
+    //this.profilePicture = "https://www.gravatar.com/avatar/" + md5(this.email.toLowerCase(), 'hex');
+    this.profilePicture = "https://www.gravatar.com/avatar/" + md5('saidllamas14@gmail.com', 'hex');
   }
 
 }
